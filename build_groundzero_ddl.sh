10e9d8dc358098c4a845e3915d96520da5a52f36
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-06-17 15:45:24 +0100

    Fix user reserved word

--- build_groundzero_ddl.sh
+++ build_groundzero_ddl.sh
@@ -16,17 +16,17 @@ else
 fi
 
 if [ -z "$UAC_QID_SERVICE_BRANCH" ]; then
-  git clone git@github.com:ONSdigital/census-rm-uac-qid-service.git
+  git clone git@github.com:ONSdigital/ssdc-rm-uac-qid-service.git
 else
   echo "Cloning UAC QID Service branch $UAC_QID_SERVICE_BRANCH"
-  git clone --branch $UAC_QID_SERVICE_BRANCH git@github.com:ONSdigital/census-rm-uac-qid-service.git
+  git clone --branch $UAC_QID_SERVICE_BRANCH git@github.com:ONSdigital/ssdc-rm-uac-qid-service.git
 fi
 
 if [ -z "$EXCEPTION_MANAGER_BRANCH" ]; then
-  git clone git@github.com:ONSdigital/census-rm-exception-manager.git
+  git clone git@github.com:ONSdigital/ssdc-rm-exception-manager.git
 else
   echo "Cloning Exception Manager branch $EXCEPTION_MANAGER_BRANCH"
-  git clone --branch $EXCEPTION_MANAGER_BRANCH git@github.com:ONSdigital/census-rm-exception-manager.git
+  git clone --branch $EXCEPTION_MANAGER_BRANCH git@github.com:ONSdigital/ssdc-rm-exception-manager.git
 fi
 
 cd ..
@@ -34,11 +34,11 @@ cd ..
 mkdir -p git_cloned_src/uk/gov/ons/ssdc/caseprocessor/model/entity
 cp temp_clone/ssdc-rm-caseprocessor/src/main/java/uk/gov/ons/ssdc/caseprocessor/model/entity/* git_cloned_src/uk/gov/ons/ssdc/caseprocessor/model/entity
 
-mkdir -p git_cloned_src/uk/gov/ons/census/uacqid/model/entity
-cp temp_clone/census-rm-uac-qid-service/src/main/java/uk/gov/ons/census/uacqid/model/entity/*.java git_cloned_src/uk/gov/ons/census/uacqid/model/entity
+mkdir -p git_cloned_src/uk/gov/ons/ssdc/uacqid/model/entity
+cp temp_clone/ssdc-rm-uac-qid-service/src/main/java/uk/gov/ons/ssdc/uacqid/model/entity/*.java git_cloned_src/uk/gov/ons/ssdc/uacqid/model/entity
 
-mkdir -p git_cloned_src/uk/gov/ons/census/exceptionmanager/model/entity
-cp temp_clone/census-rm-exception-manager/src/main/java/uk/gov/ons/census/exceptionmanager/model/entity/*.java git_cloned_src/uk/gov/ons/census/exceptionmanager/model/entity
+mkdir -p git_cloned_src/uk/gov/ons/ssdc/exceptionmanager/model/entity
+cp temp_clone/ssdc-rm-exception-manager/src/main/java/uk/gov/ons/ssdc/exceptionmanager/model/entity/*.java git_cloned_src/uk/gov/ons/ssdc/exceptionmanager/model/entity
 
 rm -rf temp_clone
 
@@ -47,5 +47,5 @@ mvn clean package
 rm -rf git_cloned_src
 
 java -jar target/ssdc-rm-ddl-1.0-SNAPSHOT.jar casev3 uk.gov.ons.ssdc.caseprocessor.model.entity
-java -jar target/ssdc-rm-ddl-1.0-SNAPSHOT.jar uacqid uk.gov.ons.census.uacqid.model.entity
-java -jar target/ssdc-rm-ddl-1.0-SNAPSHOT.jar exceptionmanager uk.gov.ons.census.exceptionmanager.model.entity
+java -jar target/ssdc-rm-ddl-1.0-SNAPSHOT.jar uacqid uk.gov.ons.ssdc.uacqid.model.entity
+java -jar target/ssdc-rm-ddl-1.0-SNAPSHOT.jar exceptionmanager uk.gov.ons.ssdc.exceptionmanager.model.entity
