3ff7a5610a593b5722aa17bb5d373b3eb57816b7
LeoKHoward <leo-howard@hotmail.co.uk>
2021-08-19 09:49:46 +0100

    Rebuilt from branches

--- groundzero_ddl/exceptionmanager.sql
+++ groundzero_ddl/exceptionmanager.sql
@@ -16,9 +16,9 @@
         headers jsonb,
         message_hash varchar(255),
         message_payload bytea,
-        queue varchar(255),
         routing_key varchar(255),
         service varchar(255),
         skipped_timestamp timestamp with time zone,
+        subscription varchar(255),
         primary key (id)
     );
