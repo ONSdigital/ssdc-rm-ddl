f4b76a50446aac829d4f882f3e5072e23f01294c
Adam <35268982+AdamHawtin@users.noreply.github.com>
2021-09-13 13:39:35 +0100

    2691 DB based print files (#26)

--- groundzero_ddl/casev3.sql
+++ groundzero_ddl/casev3.sql
@@ -137,6 +137,16 @@
         primary key (id)
     );
 
+    create table print_file_row (
+       id  bigserial not null,
+        batch_id uuid not null,
+        batch_quantity int4 not null,
+        pack_code varchar(255) not null,
+        print_supplier varchar(255) not null,
+        row varchar(255) not null,
+        primary key (id)
+    );
+
     create table print_template (
        pack_code varchar(255) not null,
         print_supplier varchar(255) not null,
