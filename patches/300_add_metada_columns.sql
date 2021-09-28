03d2062468b06d6eb21eb08aee44ba312c4ca8fa
LeoKHoward <leo-howard@hotmail.co.uk>
2021-09-28 11:08:26 +0100

    New metadata columns and patch

---
+++ patches/300_add_metada_columns.sql
@@ -0,0 +1,13 @@
+-- ****************************************************************************
+-- RM SQL DATABASE UPDATE SCRIPT
+-- ****************************************************************************
+-- Number: 300
+-- Purpose: Add metadata columns to action_rule and uac_qid_link table
+-- Author: Leo Howard
+-- ****************************************************************************
+
+ALTER TABLE casev3.action_rule
+    ADD COLUMN IF NOT EXISTS uac_metadata jsonb;
+
+ALTER TABLE casev3.uac_qid_link
+    ADD COLUMN IF NOT EXISTS metadata json;
