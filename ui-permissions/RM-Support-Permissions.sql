daa201300ea0144791ff124035dba3f57143c4e5
Adam <35268982+AdamHawtin@users.noreply.github.com>
2021-10-04 08:41:36 +0100

    2884 Add RM UI group setup scripts (#40)

---
+++ ui-permissions/RM-Support-Permissions.sql
@@ -0,0 +1,12 @@
+INSERT INTO casev3.user_group (id, name) VALUES ('b19a77bd-6a02-4851-8116-9e915738b700', 'RM SUPPORT');
+
+INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('cc542d88-b1e6-4ab0-ba09-9857a4cc5877', 'LIST_SURVEYS', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
+INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('18c78ba8-17ec-4c09-972d-c1c6c88359d5', 'VIEW_SURVEY', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
+INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('f1f526ee-a10e-4772-923b-ec3586826cbf', 'LIST_COLLECTION_EXERCISES', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
+INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('28e30f2e-2d1b-4fa1-92ca-cd8323179c55', 'VIEW_COLLECTION_EXERCISE', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
+INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('191408ca-7579-4135-a070-c5c9c3907b51', 'LIST_ALLOWED_SMS_TEMPLATES_ON_ACTION_RULES', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
+INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('53d7355d-fce6-40e1-9dd9-17af1d9a0dfc', 'SEARCH_CASES', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
+INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('91f93759-a0ba-41e6-b368-d331efb01366', 'VIEW_CASE_DETAILS', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
+INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('81e78da1-5e2a-4457-a2ee-cc12c884f2fa', 'LIST_ACTION_RULES', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
+INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('26cf0f7a-7f0f-4e5b-8be2-640745be9cdf', 'VIEW_SAMPLE_LOAD_PROGRESS', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
+INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('81395c04-416c-4e3f-9397-83a08ac8bdf2', 'LIST_SMS_TEMPLATES', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
