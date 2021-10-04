fc7b9749289c9e7b0eb433cef565fe53672590b4
AdamHawtin <adam.hawtin@ons.gov.uk>
2021-10-04 08:31:44 +0100

    Remove all the print stuff for real this time, I checked

--- ui-permissions/RM-Support-Permissions.sql
+++ ui-permissions/RM-Support-Permissions.sql
@@ -4,7 +4,6 @@ INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, sur
 INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('18c78ba8-17ec-4c09-972d-c1c6c88359d5', 'VIEW_SURVEY', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
 INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('f1f526ee-a10e-4772-923b-ec3586826cbf', 'LIST_COLLECTION_EXERCISES', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
 INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('28e30f2e-2d1b-4fa1-92ca-cd8323179c55', 'VIEW_COLLECTION_EXERCISE', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
-INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('fe549c89-961a-4191-b359-636143dc4c50', 'LIST_ALLOWED_PRINT_TEMPLATES_ON_ACTION_RULES', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
 INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('191408ca-7579-4135-a070-c5c9c3907b51', 'LIST_ALLOWED_SMS_TEMPLATES_ON_ACTION_RULES', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
 INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('53d7355d-fce6-40e1-9dd9-17af1d9a0dfc', 'SEARCH_CASES', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
 INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('91f93759-a0ba-41e6-b368-d331efb01366', 'VIEW_CASE_DETAILS', 'b19a77bd-6a02-4851-8116-9e915738b700', NULL);
