afb65c21582fa33b3e44729e59939f188f9a6f91
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2021-11-01 10:35:23 +0000

    add in new bulk permissions and job types

--- ssdc-rm-common-entity-model/src/main/java/uk/gov/ons/ssdc/common/model/entity/JobType.java
+++ ssdc-rm-common-entity-model/src/main/java/uk/gov/ons/ssdc/common/model/entity/JobType.java
@@ -2,5 +2,8 @@ package uk.gov.ons.ssdc.common.model.entity;
 
 public enum JobType {
   SAMPLE,
-  BULK_REFUSAL
+  BULK_REFUSAL,
+  BULK_UPDATE_SAMPLE_SENSITIVE,
+  BULK_INVALID,
+  BULK_UPDATE_SAMPLE
 }
