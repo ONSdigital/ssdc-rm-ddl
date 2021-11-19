4926a45704bee34e40ffeebbcbecf4e9358b8c73
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-11-19 13:19:31 +0000

    Update 500_collection_instrument_and_uac_hash.sql

--- patches/500_collection_instrument_and_uac_hash.sql
+++ patches/500_collection_instrument_and_uac_hash.sql
@@ -6,4 +6,40 @@
 -- Author: Nick Grant
 -- ****************************************************************************
 
--- TODO
+-- collection_instrument_selection_rules
+
+-- add the column
+ALTER TABLE casev3.collection_exercise
+    ADD COLUMN IF NOT EXISTS collection_instrument_selection_rules jsonb;
+
+-- give it a default value
+UPDATE casev3.collection_exercise SET collection_instrument_selection_rules = '[{"priority": 0,"spelExpression":null,"collectionInstrumentUrl": "https://raw.githubusercontent.com/ONSdigital/eq-questionnaire-schemas/main/schemas/social/en/social-demo.json"}]';
+
+-- now they all have values make it not NULL
+ALTER TABLE casev3.collection_exercise ALTER COLUMN collection_instrument_selection_rules SET NOT NULL;
+
+
+-- collection_instrument_url
+
+-- add the column
+ALTER TABLE casev3.uac_qid_link
+    ADD COLUMN IF NOT EXISTS collection_instrument_url jsonb;
+
+-- give it a default value
+UPDATE casev3.uac_qid_link SET collection_instrument_url = 'https://raw.githubusercontent.com/ONSdigital/eq-questionnaire-schemas/main/schemas/social/en/social-demo.json';
+
+-- now they all have values make it not NULL
+ALTER TABLE casev3.uac_qid_link ALTER COLUMN collection_instrument_url SET NOT NULL;
+
+
+-- uac_hash
+
+-- add the column
+ALTER TABLE casev3.uac_qid_link
+    ADD COLUMN IF NOT EXISTS uac_hash jsonb;
+
+-- give it a default value
+UPDATE casev3.uac_qid_link SET uac_hash = 'this is not a UAC hash because we need a data wipe';
+
+-- now they all have values make it not NULL
+ALTER TABLE casev3.uac_qid_link ALTER COLUMN uac_hash SET NOT NULL;
