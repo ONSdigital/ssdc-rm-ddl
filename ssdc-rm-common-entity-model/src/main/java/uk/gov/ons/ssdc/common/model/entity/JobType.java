118b5308737170746136d2124d111d6ab0009f31
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-01-26 15:38:43 +0000

    Update JobType.java

--- ssdc-rm-common-entity-model/src/main/java/uk/gov/ons/ssdc/common/model/entity/JobType.java
+++ ssdc-rm-common-entity-model/src/main/java/uk/gov/ons/ssdc/common/model/entity/JobType.java
@@ -1,9 +1,9 @@
 package uk.gov.ons.ssdc.common.model.entity;
 
 public enum JobType {
-  SAMPLE,
-  BULK_REFUSAL,
-  BULK_UPDATE_SAMPLE_SENSITIVE,
-  BULK_INVALID,
-  BULK_UPDATE_SAMPLE
+    SAMPLE,
+    BULK_REFUSAL,
+    BULK_UPDATE_SAMPLE_SENSITIVE,
+    BULK_INVALID,
+    BULK_UPDATE_SAMPLE
 }
