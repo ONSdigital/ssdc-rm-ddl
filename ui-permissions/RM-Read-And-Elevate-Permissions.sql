2bf8c18cf101a8b45dfc984d21c0fd2c2ff49751
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-10-12 09:14:59 +0100

    Create RM-Read-And-Elevate-Permissions.sql

---
+++ ui-permissions/RM-Read-And-Elevate-Permissions.sql
@@ -0,0 +1,5 @@
+INSERT INTO casev3.user_group (id, name) VALUES ('a25c7f99-d2ce-4267-aea4-0a133028f793', 'RM READ AND ELEVATE');
+
+
+INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('7dc1061b-a896-4b77-9ec6-bb21c93ba4bf', 'EXCEPTION_MANAGER_PEEK', 'e9ed16cb-8589-435b-898b-38c038bcdaaf', NULL);
+INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('a697b82c-8f1e-4111-a106-6c97c617d1fc', 'EXCEPTION_MANAGER_QUARANTINE', 'e9ed16cb-8589-435b-898b-38c038bcdaaf', NULL);
