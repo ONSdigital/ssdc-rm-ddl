1b72c1722e7534994e663fdbcf4cdca136a13b64
AdamHawtin <adam.hawtin@ons.gov.uk>
2022-10-26 13:58:26 +0100

    Add dummy patches to demonstrate test-patches in travis (MUST REVERT)

---
+++ patches/1_DUMMY_PATCH_DELETE_ME.sql
@@ -0,0 +1,1 @@
+ALTER TABLE casev3.export_file_row ADD COLUMN oops_broken_column VARCHAR(255);
---
+++ patches/2_DUMMY_PATCH_DELETE_ME.sql
@@ -0,0 +1,1 @@
+ALTER TABLE casev3.export_file_row ADD COLUMN oops_another_broken_column VARCHAR(255);
