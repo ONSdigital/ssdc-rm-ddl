7578ef138430a36ef687a53ab8c7c94dda2ebf53
liamtoozer <liam.toozer@gmail.com>
2022-12-06 08:27:26 +0000

    Have seeded data for devs

--- build_init_script.sh
+++ build_init_script.sh
@@ -20,6 +20,9 @@ echo "create schema if not exists ddl_version;"
 echo "set schema 'ddl_version';"
 cat groundzero_ddl/ddl_version.sql
 echo ""
-cat ui-permissions/RM-support-action-permissions.sql
+echo "-- Seeded Support Tool UI permissions for 'RM SUPPORT'"
 cat ui-permissions/RM-support-permissions.sql
+echo ""
+echo "-- Seeded Support Tool UI permissions for 'RM SUPPORT ACTIONS'"
+cat ui-permissions/RM-support-action-permissions.sql
 } > dev-common-postgres-image/init.sql
