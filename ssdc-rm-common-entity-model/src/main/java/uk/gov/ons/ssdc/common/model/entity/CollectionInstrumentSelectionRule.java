37e1b2f292df859aa9e628fc4a610a4f8e2dfc05
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-12-13 11:35:58 +0000

    fixed a naming mistake

--- ssdc-rm-common-entity-model/src/main/java/uk/gov/ons/ssdc/common/model/entity/CollectionInstrumentSelectionRule.java
+++ ssdc-rm-common-entity-model/src/main/java/uk/gov/ons/ssdc/common/model/entity/CollectionInstrumentSelectionRule.java
@@ -2,25 +2,28 @@ package uk.gov.ons.ssdc.common.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
+
 import lombok.Getter;
+import src.main.java.uk.gov.ons.ssdc.common.model.entity.EqLaunchSettings;
 
 @Getter
 public class CollectionInstrumentSelectionRule implements Serializable {
-  private final int priority;
-  private final String spelExpression;
-  private final String collectionInstrumentUrl;
-  private final Object eqLaunchSettings;
+    private final int priority;
+    private final String spelExpression;
+    private final String collectionInstrumentUrl;
+    private final EqLaunchSettings eqLaunchSettings;
 
-  @JsonCreator
-  public CollectionInstrumentSelectionRule(
-      @JsonProperty("priority") int priority,
-      @JsonProperty("spelExpression") String spelExpression,
-      @JsonProperty("collectionInstrumentUrl") String collectionInstrumentUrl,
-      @JsonProperty("eqLaunchData") Object EqLaunchSettings) {
-    this.priority = priority;
-    this.spelExpression = spelExpression;
-    this.collectionInstrumentUrl = collectionInstrumentUrl;
-    this.eqLaunchSettings = EqLaunchSettings;
-  }
+    @JsonCreator
+    public CollectionInstrumentSelectionRule(
+            @JsonProperty("priority") int priority,
+            @JsonProperty("spelExpression") String spelExpression,
+            @JsonProperty("collectionInstrumentUrl") String collectionInstrumentUrl,
+            @JsonProperty("eqLaunchSettings") EqLaunchSettings EqLaunchSettings) {
+        this.priority = priority;
+        this.spelExpression = spelExpression;
+        this.collectionInstrumentUrl = collectionInstrumentUrl;
+        this.eqLaunchSettings = EqLaunchSettings;
+    }
 }
