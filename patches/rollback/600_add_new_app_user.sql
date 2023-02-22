aac65d531bbc3dffde1c820e2c020507c1284413
KieranWardle <23309814+KieranWardle@users.noreply.github.com>
2023-02-22 16:51:53 +0000

    Fixed rollback patch

--- patches/rollback/600_add_new_app_user.sql
+++ patches/rollback/600_add_new_app_user.sql
@@ -2,7 +2,7 @@
 -- RM SQL DATABASE INSERT ROLLBACK SCRIPT
 -- ****************************************************************************
 -- Number: 600
--- Purpose: Rollback adding new rm_app_user role to new rm_app_user user
+-- Purpose: Rollback adding new rm_app_user role FROM new rm_app_user user
 -- Author: Kieran Wardle
 -- ****************************************************************************
 
@@ -10,5 +10,19 @@ REVOKE rm_app_user FROM appuser;
 GRANT cloudsqlsuperuser TO appuser;
 ALTER ROLE appuser WITH CREATEDB;
 ALTER ROLE appuser WITH CREATEROLE;
+
+REVOKE SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA casev3 FROM rm_app_user;
+REVOKE SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA exceptionmanager FROM rm_app_user;
+REVOKE SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA uacqid FROM rm_app_user;
+
+REVOKE SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA casev3 FROM rm_app_user;
+REVOKE SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA uacqid FROM rm_app_user;
+REVOKE SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA exceptionmanager FROM rm_app_user;
+
+REVOKE CONNECT ON DATABASE rm FROM rm_app_user;
+
+REVOKE USAGE ON SCHEMA casev3 FROM rm_app_user;
+REVOKE USAGE ON SCHEMA exceptionmanager FROM rm_app_user;
+REVOKE USAGE ON SCHEMA uacqid FROM rm_app_user;
 
 DROP ROLE rm_app_user;
