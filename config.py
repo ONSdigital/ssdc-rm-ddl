b460b1c09e29ad835b51787308943965fdf141b5
Kieran Wardle <23309814+KieranWardle@users.noreply.github.com>
2023-02-23 11:20:00 +0000

    550 create a separate application database user (#120)

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 
 class Config:
-    DB_USERNAME = os.getenv('DB_USERNAME', 'postgres')
+    DB_USERNAME = os.getenv('DB_USERNAME', 'appuser')
     DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
     DB_HOST = os.getenv('DB_HOST', 'localhost')
     DB_PORT = os.getenv('DB_PORT', '6432')
-    DB_NAME = os.getenv('DB_NAME', 'postgres')
+    DB_NAME = os.getenv('DB_NAME', 'rm')
     DB_USESSL = os.getenv('DB_USESSL', '')
