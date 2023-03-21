6d70b0bbfb9721dc0d02f2b6a48f2c4f001232a5
Liam Toozer <liam.toozer@gmail.com>
2023-03-21 17:17:31 +0000

    Add semicolons

--- patches/rollback/800_delete_phm_templates_and_insert_cris_templates.sql
+++ patches/rollback/800_delete_phm_templates_and_insert_cris_templates.sql
@@ -19,7 +19,7 @@ VALUES
     ('NL_EN_ST_PHM', 'Notification Letter - English', 'internal_reprographics', null ,'["PARTICIPANT_ID","ADDRESS_LINE_1","ADDRESS_LINE_2","ADDRESS_LINE_3","TOWN_NAME","POSTCODE","__sensitive__.LAST_NAME","__sensitive__.MIDDLE_NAME","FIRST_NAME","__uac__","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE"]'),
     ('NL_CY_ST_PHM', 'Notification Letter - Welsh', 'internal_reprographics', null ,'["PARTICIPANT_ID","ADDRESS_LINE_1","ADDRESS_LINE_2","ADDRESS_LINE_3","TOWN_NAME","POSTCODE","__sensitive__.LAST_NAME","__sensitive__.MIDDLE_NAME","FIRST_NAME","__uac__","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE"]'),
     ('NL_EN_LP_PHM', 'Notification Letter - English - Large Print', 'internal_reprographics', null ,'["PARTICIPANT_ID","ADDRESS_LINE_1","ADDRESS_LINE_2","ADDRESS_LINE_3","TOWN_NAME","POSTCODE","__sensitive__.LAST_NAME","__sensitive__.MIDDLE_NAME","FIRST_NAME","__uac__","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE"]'),
-    ('NL_CY_LP_PHM', 'Notification Letter - Welsh - Large Print', 'internal_reprographics', null ,'["PARTICIPANT_ID","ADDRESS_LINE_1","ADDRESS_LINE_2","ADDRESS_LINE_3","TOWN_NAME","POSTCODE","__sensitive__.LAST_NAME","__sensitive__.MIDDLE_NAME","FIRST_NAME","__uac__","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE"]')
+    ('NL_CY_LP_PHM', 'Notification Letter - Welsh - Large Print', 'internal_reprographics', null ,'["PARTICIPANT_ID","ADDRESS_LINE_1","ADDRESS_LINE_2","ADDRESS_LINE_3","TOWN_NAME","POSTCODE","__sensitive__.LAST_NAME","__sensitive__.MIDDLE_NAME","FIRST_NAME","__uac__","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE"]');
 
 
 INSERT INTO casev3.email_template (pack_code, description, notify_template_id, metadata, template)
@@ -31,4 +31,4 @@ VALUES
     ('RE_EN_PHM', 'Reminder Email - English', '7ae006fa-1917-4299-ad7c-c8f8d0ecd2d4', null ,'["PARTICIPANT_ID","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.MIDDLE_NAME","__sensitive__.LAST_NAME"]'),
     ('RE_CY_PHM', 'Reminder Email - Welsh', 'f782df1a-0204-4e95-a7e0-e64ac8c7270d', null ,'["PARTICIPANT_ID","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.MIDDLE_NAME","__sensitive__.LAST_NAME"]'),
     ('UR_EN_PHM', 'UAC Replacement Email - English', '239e0ef7-3e1d-4f73-96b7-ac06ce6b7fc4', null ,'["__uac__","FIRST_NAME","__sensitive__.MIDDLE_NAME","__sensitive__.LAST_NAME"]'),
-    ('UR_CY_PHM', 'UAC Replacement Email - Welsh', 'e928f6f1-dd04-4d2c-a780-97db65866643', null ,'["__uac__","FIRST_NAME","__sensitive__.MIDDLE_NAME","__sensitive__.LAST_NAME"]')
+    ('UR_CY_PHM', 'UAC Replacement Email - Welsh', 'e928f6f1-dd04-4d2c-a780-97db65866643', null ,'["__uac__","FIRST_NAME","__sensitive__.MIDDLE_NAME","__sensitive__.LAST_NAME"]');
