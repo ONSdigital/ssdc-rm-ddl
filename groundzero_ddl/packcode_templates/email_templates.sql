c5cf5bb39c33face2ef950d500eae3d6f8104e9e
liamtoozer <liam.toozer@gmail.com>
2023-03-23 08:42:40 +0000

    Update groundzero_ddl/packcode_templates/email_templates.sql
    Co-authored-by: Adam <35268982+AdamHawtin@users.noreply.github.com>

--- groundzero_ddl/packcode_templates/email_templates.sql
+++ groundzero_ddl/packcode_templates/email_templates.sql
@@ -1,5 +1,5 @@
 INSERT INTO casev3.email_template (pack_code, description, notify_template_id, metadata, template) VALUES
-('NE_EN_CRIS', 'CRIS Notification- English', '55c23289-6331-47f2-89b9-5b88bcfc3f41', null ,'["__uac__","PORTAL_ID","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
+('NE_EN_CRIS', 'CRIS Notification - English', '55c23289-6331-47f2-89b9-5b88bcfc3f41', null ,'["__uac__","PORTAL_ID","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
 ('NE_CY_CRIS', 'CRIS Notification - bilingual', 'acd97184-b282-4ff9-98cf-ba14bf74c2f8', null ,'["__uac__","PORTAL_ID","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
 ('RE_EN_CRIS', 'CRIS Reminder - English', '8cf30001-0c06-4da4-b49d-1df975a1123a', null ,'["PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
 ('RE_CY_CRIS', 'CRIS Reminder - bilingual', '42af756c-7239-4486-b67d-ce2c6c4a7f9b', null ,'["PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
