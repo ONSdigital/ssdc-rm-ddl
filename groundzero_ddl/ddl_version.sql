0d3bb50b8a592017f54e6d372ff291094d46231c
Adam <35268982+AdamHawtin@users.noreply.github.com>
2023-04-04 15:18:47 +0100

    886 Update CRIS export file templates (#130)

--- groundzero_ddl/ddl_version.sql
+++ groundzero_ddl/ddl_version.sql
@@ -4,5 +4,5 @@ CREATE TABLE ddl_version.version (version_tag varchar(256) PRIMARY KEY, updated_
 -- Version and patch number for the current ground zero,
 -- NOTE: These must be updated every time the repo is tagged
 -- NOTE: the CURRENT_VERSION in /patch_database.py must also be updated to match this version_tag
-INSERT INTO ddl_version.patches (patch_number, applied_timestamp) VALUES (900, current_timestamp);
-INSERT INTO ddl_version.version (version_tag, updated_timestamp) VALUES ('v1.0.9', current_timestamp);
+INSERT INTO ddl_version.patches (patch_number, applied_timestamp) VALUES (1000, current_timestamp);
+INSERT INTO ddl_version.version (version_tag, updated_timestamp) VALUES ('v1.0.10', current_timestamp);
---
+++ patches/rollback/1000_update_CRIS_export_file_templates.sql
@@ -0,0 +1,11 @@
+-- ****************************************************************************
+-- RM SQL DATABASE UPDATE SCRIPT
+-- ****************************************************************************
+-- Number: 1000
+-- Purpose: Update the CRIS export file templates
+-- Author: Adam Hawtin
+-- ****************************************************************************
+
+UPDATE casev3.export_file_template
+SET template =  '["PORTAL_ID","ADDRESS_LINE_1","ADDRESS_LINE_2","ADDRESS_LINE_3","TOWN_NAME","POSTCODE","__sensitive__.LAST_NAME","FIRST_NAME","__uac__","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE"]'
+WHERE pack_code IN ('NL_EN_ST_CRIS','NL_CY_ST_CRIS','NL_EN_LP_CRIS','NL_CY_LP_CRIS');
