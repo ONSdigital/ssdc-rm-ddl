4ba0ed0fd2002212221cd8e8afc2efef3907bcb1
AdamHawtin <adam.hawtin@ons.gov.uk>
2023-04-04 14:48:51 +0100

    Re-include name fields, they are still required

--- patches/1000_update_CRIS_export_file_templates.sql
+++ patches/1000_update_CRIS_export_file_templates.sql
@@ -7,5 +7,5 @@
 -- ****************************************************************************
 
 UPDATE casev3.export_file_template
-SET template = '["PORTAL_ID","__sensitive__.ADDRESS_LINE_1","ADDRESS_LINE_2","ADDRESS_LINE_3","TOWN_NAME","POSTCODE","__uac__","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE"]'
+SET template = '["PORTAL_ID","__sensitive__.ADDRESS_LINE_1","ADDRESS_LINE_2","ADDRESS_LINE_3","TOWN_NAME","POSTCODE","__sensitive__.LAST_NAME","FIRST_NAME","__uac__","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE"]'
 WHERE pack_code IN ('NL_EN_ST_CRIS','NL_CY_ST_CRIS','NL_EN_LP_CRIS','NL_CY_LP_CRIS');
