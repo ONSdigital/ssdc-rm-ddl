8ae9f3935e5ab4ee01fcbcdffe62b8b05ca711d0
hbrace <43209528+hbrace@users.noreply.github.com>
2023-05-03 12:37:33 +0100

    Update groundzero_ddl/packcode_templates/email_templates.sql
    Co-authored-by: Adam <35268982+AdamHawtin@users.noreply.github.com>

--- groundzero_ddl/packcode_templates/email_templates.sql
+++ groundzero_ddl/packcode_templates/email_templates.sql
@@ -6,5 +6,5 @@ INSERT INTO casev3.email_template (pack_code, description, notify_template_id, m
 ('UR_EN_CRIS', 'CRIS UAC Replacement - English', '3543bcf5-6986-4d8d-80ab-79fba4d3f050', null ,'["__uac__","PORTAL_ID","FIRST_NAME","__sensitive__.LAST_NAME"]'),
 ('UR_CY_CRIS', 'CRIS UAC Replacement - Bilingual Welsh', 'cfe91e5d-19a9-4325-8e4f-0bcbd1dcacff', null ,'["__uac__","PORTAL_ID","FIRST_NAME","__sensitive__.LAST_NAME"]'),
 ('RE_UAC_EN_CRIS', 'CRIS Reminder with UAC - English', 'd0224400-4894-4bdd-b0bc-839972641327', null, '["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
-('RE_UAC_CY_CRIS', 'CRIS Reminder with UAC - bilingual', 'bc35ebcd-63ca-4a90-aa4c-eea3e85ed506', null, '["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]')
+('RE_UAC_CY_CRIS', 'CRIS Reminder with UAC - Bilingual Welsh', 'bc35ebcd-63ca-4a90-aa4c-eea3e85ed506', null, '["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]')
 ON CONFLICT (pack_code) DO UPDATE SET (description, notify_template_id, metadata, template) = (EXCLUDED.description, EXCLUDED.notify_template_id, EXCLUDED.metadata, EXCLUDED.template);
