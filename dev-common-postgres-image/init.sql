032e58b31c287349e5d2f4ab798c451be01cb859
hbrace <43209528+hbrace@users.noreply.github.com>
2023-05-03 13:32:13 +0100

    Add CRIS reminders with UAC email templates (#133)
    Co-authored-by: Adam <35268982+AdamHawtin@users.noreply.github.com>

--- dev-common-postgres-image/init.sql
+++ dev-common-postgres-image/init.sql
@@ -519,8 +519,8 @@ CREATE TABLE ddl_version.version (version_tag varchar(256) PRIMARY KEY, updated_
 -- Version and patch number for the current ground zero,
 -- NOTE: These must be updated every time the repo is tagged
 -- NOTE: the CURRENT_VERSION in /patch_database.py must also be updated to match this version_tag
-INSERT INTO ddl_version.patches (patch_number, applied_timestamp) VALUES (1000, current_timestamp);
-INSERT INTO ddl_version.version (version_tag, updated_timestamp) VALUES ('v1.0.10', current_timestamp);
+INSERT INTO ddl_version.patches (patch_number, applied_timestamp) VALUES (1100, current_timestamp);
+INSERT INTO ddl_version.version (version_tag, updated_timestamp) VALUES ('v1.0.11', current_timestamp);
 
 -- Seed Support Tool UI permissions
 BEGIN;
@@ -609,5 +609,7 @@ INSERT INTO casev3.email_template (pack_code, description, notify_template_id, m
 ('RE_EN_CRIS', 'CRIS Reminder - English', '8cf30001-0c06-4da4-b49d-1df975a1123a', null ,'["PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
 ('RE_CY_CRIS', 'CRIS Reminder - Bilingual Welsh', '42af756c-7239-4486-b67d-ce2c6c4a7f9b', null ,'["PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
 ('UR_EN_CRIS', 'CRIS UAC Replacement - English', '3543bcf5-6986-4d8d-80ab-79fba4d3f050', null ,'["__uac__","PORTAL_ID","FIRST_NAME","__sensitive__.LAST_NAME"]'),
-('UR_CY_CRIS', 'CRIS UAC Replacement - Bilingual Welsh', 'cfe91e5d-19a9-4325-8e4f-0bcbd1dcacff', null ,'["__uac__","PORTAL_ID","FIRST_NAME","__sensitive__.LAST_NAME"]')
+('UR_CY_CRIS', 'CRIS UAC Replacement - Bilingual Welsh', 'cfe91e5d-19a9-4325-8e4f-0bcbd1dcacff', null ,'["__uac__","PORTAL_ID","FIRST_NAME","__sensitive__.LAST_NAME"]'),
+('RE_UAC_EN_CRIS', 'CRIS Reminder with UAC - English', 'd0224400-4894-4bdd-b0bc-839972641327', null, '["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
+('RE_UAC_CY_CRIS', 'CRIS Reminder with UAC - bilingual', 'bc35ebcd-63ca-4a90-aa4c-eea3e85ed506', null, '["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]')
 ON CONFLICT (pack_code) DO UPDATE SET (description, notify_template_id, metadata, template) = (EXCLUDED.description, EXCLUDED.notify_template_id, EXCLUDED.metadata, EXCLUDED.template);
