1c23aa191e1870f96ee02dc2942e7e2d3878cc4c
AdamHawtin <adam.hawtin@ons.gov.uk>
2023-10-16 10:09:51 +0100

    Fix rogue comma

--- patches/1400_add_WCIS_email_templates.sql
+++ patches/1400_add_WCIS_email_templates.sql
@@ -11,5 +11,5 @@ INSERT INTO casev3.email_template (pack_code, description, notify_template_id, m
 ('MR1_EN_WCIS', 'WCIS Main Reminder Email 1 - English', '179b306c-ac35-4736-8804-94e8daaef511', null ,'["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
 ('MR2_EN_WCIS', 'WCIS Main Reminder Email 2 - English', 'ff5f26e5-eeb6-4cdc-a93f-ee8a5b3e19e3', null ,'["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
 ('FNE_EN_WCIS', 'WCIS Follow-up Notification Email - English', 'cdf7b57a-46d9-47cc-aea9-a6aaa3c153ba', null ,'["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
-('FRE_EN_WCIS', 'WCIS Follow-up Reminder Email - English', 'd66911c6-7323-4362-8eca-e5d2bee4d9f2', null ,'["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
+('FRE_EN_WCIS', 'WCIS Follow-up Reminder Email - English', 'd66911c6-7323-4362-8eca-e5d2bee4d9f2', null ,'["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]')
 ON CONFLICT (pack_code) DO UPDATE SET (description, notify_template_id, metadata, template) = (EXCLUDED.description, EXCLUDED.notify_template_id, EXCLUDED.metadata, EXCLUDED.template);
