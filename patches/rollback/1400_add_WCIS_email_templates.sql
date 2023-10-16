a7c6de7ca7c3d75ab73247c872da1ba40e2a4871
AdamHawtin <adam.hawtin@ons.gov.uk>
2023-10-16 08:55:45 +0100

    Update DDL versions, fix rollback script

--- patches/rollback/1400_add_WCIS_email_templates.sql
+++ patches/rollback/1400_add_WCIS_email_templates.sql
@@ -6,10 +6,5 @@
 -- Author: Adam Hawtin
 -- ****************************************************************************
 
-INSERT INTO casev3.email_template (pack_code, description, notify_template_id, metadata, template) VALUES
-('MNE_EN_WCIS', 'WCIS Main Notification Email - English', '6ff6a02f-f4ee-49ca-ba6c-4cabb1e72688', null ,'["__uac__","PORTAL_ID","COLLEX_OPEN_DATE","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
-('MR1_EN_WCIS', 'WCIS Main Reminder Email 1 - English', '179b306c-ac35-4736-8804-94e8daaef511', null ,'["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
-('MR2_EN_WCIS', 'WCIS Main Reminder Email 2 - English', 'ff5f26e5-eeb6-4cdc-a93f-ee8a5b3e19e3', null ,'["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
-('FNE_EN_WCIS', 'WCIS Follow-up Notification Email - English', 'cdf7b57a-46d9-47cc-aea9-a6aaa3c153ba', null ,'["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
-('FRE_EN_WCIS', 'WCIS Follow-up Reminder Email - English', 'd66911c6-7323-4362-8eca-e5d2bee4d9f2', null ,'["__uac__","PORTAL_ID","COLLEX_CLOSE_DATE","FIRST_NAME","__sensitive__.LAST_NAME"]'),
-ON CONFLICT (pack_code) DO UPDATE SET (description, notify_template_id, metadata, template) = (EXCLUDED.description, EXCLUDED.notify_template_id, EXCLUDED.metadata, EXCLUDED.template);
+DELETE FROM casev3.email_template
+WHERE pack_code IN ('MNE_EN_WCIS', 'MR1_EN_WCIS', 'MR2_EN_WCIS', 'FNE_EN_WCIS', 'FRE_EN_WCIS');
