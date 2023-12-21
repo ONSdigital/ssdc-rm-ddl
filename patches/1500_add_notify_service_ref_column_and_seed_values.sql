7817ded9862d82ec93dbaa2ef95531f4e478a1fa
Adam <35268982+AdamHawtin@users.noreply.github.com>
2023-12-21 10:56:33 +0000

    1043 Add Notify Service Ref Columns (#153)
    Co-authored-by: Ryan <ryan.grundy@ons.gov.uk>

---
+++ patches/1500_add_notify_service_ref_column_and_seed_values.sql
@@ -0,0 +1,13 @@
+-- ****************************************************************************
+-- RM SQL DATABASE INSERT SCRIPT
+-- ****************************************************************************
+-- Number: 1500
+-- Purpose: Add Notify service ref columns to email and SMS templates, update existing rows
+-- Author: Adam Hawtin
+-- ****************************************************************************
+
+ALTER TABLE casev3.email_template ADD notify_service_ref VARCHAR(255);
+ALTER TABLE casev3.sms_template ADD notify_service_ref VARCHAR(255);
+
+UPDATE casev3.email_template SET notify_service_ref = 'Office_for_National_Statistics_surveys_UKHSA';
+UPDATE casev3.sms_template SET notify_service_ref = 'Office_for_National_Statistics_surveys_UKHSA';
