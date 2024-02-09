5d860d2184583acb5cb2075af5f5d07ff7181353
Ryan Grundy <ryan.grundy@ons.gov.uk>
2024-02-09 15:33:36 +0000

    Replicated current behaviour with jobrow table

--- ssdc-rm-common-entity-model/src/main/java/uk/gov/ons/ssdc/common/model/entity/JobRow.java
+++ ssdc-rm-common-entity-model/src/main/java/uk/gov/ons/ssdc/common/model/entity/JobRow.java
@@ -2,9 +2,7 @@ package uk.gov.ons.ssdc.common.model.entity;
 
 import io.hypersistence.utils.hibernate.type.json.JsonBinaryType;
 import jakarta.persistence.*;
-
 import java.nio.charset.Charset;
-import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
 import lombok.Data;
@@ -29,7 +27,7 @@ public class JobRow {
   @Lob
   @JdbcTypeCode(SqlTypes.VARBINARY)
   @Column(nullable = false)
-  private byte[] originalRowData;
+  private byte[][] originalRowData;
 
   @Column(nullable = false)
   private int originalRowLineNumber;
@@ -60,10 +58,28 @@ public class JobRow {
   }
 
   public String[] getOriginalRowData() {
-    return new String[]{Arrays.toString(this.originalRowData)};
+
+    return convertToStrings(originalRowData);
   }
 
   public void setOriginalRowData(String[] originalRowDataStr) {
-    this.originalRowData = Arrays.toString(originalRowDataStr).getBytes();
+    originalRowData = convertToBytes(originalRowDataStr);
+  }
+
+  private static String[] convertToStrings(byte[][] byteStrings) {
+    String[] data = new String[byteStrings.length];
+    for (int i = 0; i < byteStrings.length; i++) {
+      data[i] = new String(byteStrings[i], Charset.defaultCharset());
+    }
+    return data;
+  }
+
+  private static byte[][] convertToBytes(String[] strings) {
+    byte[][] data = new byte[strings.length][];
+    for (int i = 0; i < strings.length; i++) {
+      String string = strings[i];
+      data[i] = string.getBytes(Charset.defaultCharset()); // you can chose charset
+    }
+    return data;
   }
 }
