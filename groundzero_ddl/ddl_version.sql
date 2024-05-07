03d1548470101ba7139cca1a48def288a3a9d00f
Ryan Grundy <ryan.grundy@ons.gov.uk>
2024-05-07 11:34:56 +0100

    SDCSRM-368 removing WCIS templates and resetting ddl

--- groundzero_ddl/ddl_version.sql
+++ groundzero_ddl/ddl_version.sql
@@ -4,5 +4,5 @@ CREATE TABLE ddl_version.version (version_tag varchar(256) PRIMARY KEY, updated_
 -- Version and patch number for the current ground zero,
 -- NOTE: These must be updated every time the repo is tagged
 -- NOTE: the CURRENT_VERSION in /patch_database.py must also be updated to match this version_tag
-INSERT INTO ddl_version.patches (patch_number, applied_timestamp) VALUES (1700, current_timestamp);
-INSERT INTO ddl_version.version (version_tag, updated_timestamp) VALUES ('v1.2.0', current_timestamp);
+INSERT INTO ddl_version.patches (patch_number, applied_timestamp) VALUES (0, current_timestamp);
+INSERT INTO ddl_version.version (version_tag, updated_timestamp) VALUES ('v1.0.0', current_timestamp);
--- patches/300_add_create_eq_flush_action_rule_permission.sql
+++
@@ -1,9 +0,0 @@
--- ****************************************************************************
--- RM SQL DATABASE INSERT SCRIPT
--- ****************************************************************************
--- Number: 300
--- Purpose: Add create EQ flush action rule permission to RM support action group
--- Author: Adam Hawtin
--- ****************************************************************************
-
-INSERT INTO casev3.user_group_permission (id, authorised_activity, group_id, survey_id) VALUES ('aa9c83d5-beac-48c3-b32e-9851a057bc83', 'CREATE_EQ_FLUSH_ACTION_RULE', 'a25c7f99-d2ce-4267-aea4-0a133028f793', NULL) ON CONFLICT DO NOTHING;
