fde8ea4aad6645bc7813d47e9b0551d940952b49
ryangrundy7 <35301276+ryangrundy7@users.noreply.github.com>
2025-05-21 13:00:46 +0100

    Sdcsrm 982 update rm app user permissions on tables (#224)

--- dev-common-postgres-image/init.sql
+++ dev-common-postgres-image/init.sql
@@ -521,8 +521,8 @@ CREATE TABLE ddl_version.version (version_tag varchar(256) PRIMARY KEY, updated_
 -- Version and patch number for the current ground zero,
 -- NOTE: These must be updated every time the repo is tagged
 -- NOTE: the CURRENT_VERSION in /patch_database.py must also be updated to match this version_tag
-INSERT INTO ddl_version.patches (patch_number, applied_timestamp) VALUES (600, current_timestamp);
-INSERT INTO ddl_version.version (version_tag, updated_timestamp) VALUES ('v1.3.2', current_timestamp);
+INSERT INTO ddl_version.patches (patch_number, applied_timestamp) VALUES (700, current_timestamp);
+INSERT INTO ddl_version.version (version_tag, updated_timestamp) VALUES ('v1.3.3', current_timestamp);
 
 -- Seed Support Tool UI permissions
 BEGIN;
