3dd708339a9378ea55c90726224ba81d7ed14b08
Serena <serena.m.f.crocker@gmail.com>
2026-05-06 15:30:23 +0100

    Missed casev3

--- patches/1500_create_action_template_tables.sql
+++ patches/1500_create_action_template_tables.sql
@@ -22,9 +22,9 @@ CREATE TABLE casev3.action_template_row (
     action_template_id uuid NOT NULL,
     email_template_pack_code varchar(255),
     PRIMARY KEY (id),
-    CONSTRAINT fk_action_template_row_action_template FOREIGN KEY (action_template_id) REFERENCES action_template (id),
-    CONSTRAINT fk_action_template_row_email_template FOREIGN KEY (email_template_pack_code) REFERENCES email_template (pack_code)
+    CONSTRAINT fk_action_template_row_action_template FOREIGN KEY (action_template_id) REFERENCES casev3.action_template (id),
+    CONSTRAINT fk_action_template_row_email_template FOREIGN KEY (email_template_pack_code) REFERENCES casev3.email_template (pack_code)
 );
 
 ALTER TABLE IF EXISTS casev3.collection_exercise ADD COLUMN template_id uuid;
-ALTER TABLE IF EXISTS casev3.collection_exercise ADD CONSTRAINT fk_collection_exercise_action_template FOREIGN KEY (template_id) REFERENCES action_template (id);
+ALTER TABLE IF EXISTS casev3.collection_exercise ADD CONSTRAINT fk_collection_exercise_action_template FOREIGN KEY (template_id) REFERENCES casev3.action_template (id);
